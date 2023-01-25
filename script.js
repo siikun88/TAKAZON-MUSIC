b6c3886b6ef2c068efabbe1c9dcdad3b115493fb
siikun88 <98212936+siikun88@users.noreply.github.com>
2023-01-25 20:10:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-const otowakka = new Audio('audio/sinnkou.mp3');
-const sinnkou = new Audio('audio/sinnkou.mp3');
-const sushitabetai = new Audio('audio/sushitabetai.mp3');
-const teikyouheiseidaigaku = new Audio('audio/teikyouheiseidaigaku.mp3');
-const simazirou = new Audio('audio/simazirou.mp3');
-const daisuke = new Audio('audio/daisuke.mp3');
+var otowakka = new Audio("https://drive.google.com/u/0/uc?id=1KJWXY7yRMJNdcngomMePXEadlOyNk8I3&export=download");
+var daisuke = new Audio("https://drive.google.com/u/0/uc?id=1LOkIjPRuesWqwxwlNArfmR5jYP_A4tJf&export=download");
+var teikyou = new Audio("https://drive.google.com/u/0/uc?id=1mOzD7iWpY4W_cc_3BeQwmglhHSLrL2hu&export=download");
+var sushi = new Audio("https://drive.google.com/u/0/uc?id=1Q1ddcpbWLmMTgVMguYkr0piMsaA4koyB&export=download");
+var happy = new Audio("https://drive.google.com/u/0/uc?id=1037m0V1GtTiCyOdgW9hxeTtDcSjlWsYS&export=download");
+var kannnamu = new Audio("https://drive.google.com/u/0/uc?id=1sB22tGsTnYb58X3cNyM6i7NMswyezTJL&export=download");
 function clickListener (e) {
-    if (e.target.getAttribute("id") === "帝京平成大学"){
-        teikyouheiseidaigaku.play();
-    }else if (e.target.getAttribute("id") === "閃光のハサウェイ"){
-        sinnkou.currentTime = 0;
-        sinnkou.play();
-    }else if (e.target.getAttribute("id") === "寿司食べたい"){
-        sushitabetai.currentTime = 0;
-        sushitabetai.play();
-    }else if (e.target.getAttribute("id") === "ハッピージャムジャム"){
-        simazirou.currentTime = 0;
-        simazirou.play();
-    }else if (e.target.getAttribute("id") === "おとわっか"){
-        otowakka.currentTime = 0;
-        otowakka.play();
-    }else if (e.target.getAttribute("id") === "ダイスケ"){
-        daisuke.currentTime = 0;
-        daisuke.play();
-    }
+  if (e.target.getAttribute("id") === "帝京平成大学"){
+    teikyou.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else if (e.target.getAttribute("id") === "寿司食べたい"){
+    sushi.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else if (e.target.getAttribute("id") === "ハッピージャムジャム"){
+    happy.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else if (e.target.getAttribute("id") === "おとわっか"){
+    otowakka.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else if (e.target.getAttribute("id") === "ダイスケ"){
+    daisuke.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else if (e.target.getAttribute("id") === "江南スタイル"){
+    kannnamu.play();
+    alert( e.target.getAttribute("id") + "を再生中です。" )
+  }else{
+    alert("エラーが発生しました。サイトを再読み込みして、もう一度試してみてください。")
   }
-  document.querySelectorAll("img").forEach((imgElm) => {
-    imgElm.addEventListener('click', clickListener);
-  })
+}
+document.querySelectorAll("img").forEach((imgElm) => {
+  imgElm.addEventListener('click', clickListener);
+})
